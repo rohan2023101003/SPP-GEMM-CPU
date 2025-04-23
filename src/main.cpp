67446f4bb6f9997718d799948108b31acfc94867
rohan2023101003 <rohankumar992p@gmail.com>
2025-04-23 22:57:32 +0530

    21

--- src/main.cpp
+++ src/main.cpp
@@ -55,68 +55,73 @@ namespace solution {
         }
 
         const int BN = 48, BM = 48, BK = 960;
+        const int num_blocks_n = (n + BN - 1) / BN;
+        const int num_blocks_m = (m + BM - 1) / BM;
+        const int total_blocks = num_blocks_n * num_blocks_m;
 
-        #pragma omp parallel for collapse(2) schedule(static)
-        for (int i_block = 0; i_block < n; i_block += BN) {
-            for (int j_block = 0; j_block < m; j_block += BM) {
-                float local_block[BN][BM] = {0};
+        #pragma omp parallel for schedule(static) proc_bind(close)
+        for (int block_idx = 0; block_idx < total_blocks; ++block_idx) {
+            int i_block = (block_idx / num_blocks_m) * BN;
+            int j_block = (block_idx % num_blocks_m) * BM;
 
-                for (int k_block = 0; k_block < k; k_block += BK) {
-                    int i_end = std::min(i_block + BN, n);
-                    int j_end = std::min(j_block + BM, m);
-                    int k_end = std::min(k_block + BK, k);
+            float local_block[BN][BM] = {0};
 
-                    for (int i = i_block; i < i_end; i++) {
-                        int i_rel = i - i_block;
-                        for (int j = j_block; j < j_end; j++) {
-                            int j_rel = j - j_block;
+            int i_end = std::min(i_block + BN, n);
+            int j_end = std::min(j_block + BM, m);
 
-                            __m512 sum0 = _mm512_setzero_ps();
-                            __m512 sum1 = _mm512_setzero_ps();
-                            __m512 sum2 = _mm512_setzero_ps();
-                            __m512 sum3 = _mm512_setzero_ps();
+            for (int k_block = 0; k_block < k; k_block += BK) {
+                int k_end = std::min(k_block + BK, k);
 
-                            int l = k_block;
-                            for (; l + 63 < k_end; l += 64) {
-                                __m512 a0 = _mm512_loadu_ps(&m1[i * k + l]);
-                                __m512 b0 = _mm512_loadu_ps(&m2t[j * k + l]);
-                                sum0 = _mm512_fmadd_ps(a0, b0, sum0);
+                for (int i = i_block; i < i_end; i++) {
+                    int i_rel = i - i_block;
+                    for (int j = j_block; j < j_end; j++) {
+                        int j_rel = j - j_block;
 
-                                __m512 a1 = _mm512_loadu_ps(&m1[i * k + l + 16]);
-                                __m512 b1 = _mm512_loadu_ps(&m2t[j * k + l + 16]);
-                                sum1 = _mm512_fmadd_ps(a1, b1, sum1);
+                        __m512 sum0 = _mm512_setzero_ps();
+                        __m512 sum1 = _mm512_setzero_ps();
+                        __m512 sum2 = _mm512_setzero_ps();
+                        __m512 sum3 = _mm512_setzero_ps();
 
-                                __m512 a2 = _mm512_loadu_ps(&m1[i * k + l + 32]);
-                                __m512 b2 = _mm512_loadu_ps(&m2t[j * k + l + 32]);
-                                sum2 = _mm512_fmadd_ps(a2, b2, sum2);
+                        int l = k_block;
+                        for (; l + 63 < k_end; l += 64) {
+                            __m512 a0 = _mm512_loadu_ps(&m1[i * k + l]);
+                            __m512 b0 = _mm512_loadu_ps(&m2t[j * k + l]);
+                            sum0 = _mm512_fmadd_ps(a0, b0, sum0);
 
-                                __m512 a3 = _mm512_loadu_ps(&m1[i * k + l + 48]);
-                                __m512 b3 = _mm512_loadu_ps(&m2t[j * k + l + 48]);
-                                sum3 = _mm512_fmadd_ps(a3, b3, sum3);
-                            }
+                            __m512 a1 = _mm512_loadu_ps(&m1[i * k + l + 16]);
+                            __m512 b1 = _mm512_loadu_ps(&m2t[j * k + l + 16]);
+                            sum1 = _mm512_fmadd_ps(a1, b1, sum1);
 
-                            for (; l + 15 < k_end; l += 16) {
-                                __m512 a = _mm512_loadu_ps(&m1[i * k + l]);
-                                __m512 b = _mm512_loadu_ps(&m2t[j * k + l]);
-                                sum0 = _mm512_fmadd_ps(a, b, sum0);
-                            }
+                            __m512 a2 = _mm512_loadu_ps(&m1[i * k + l + 32]);
+                            __m512 b2 = _mm512_loadu_ps(&m2t[j * k + l + 32]);
+                            sum2 = _mm512_fmadd_ps(a2, b2, sum2);
 
-                            float sum = _mm512_reduce_add_ps(_mm512_add_ps(
-                                _mm512_add_ps(sum0, sum1), _mm512_add_ps(sum2, sum3)));
-
-                            for (; l < k_end; l++) {
-                                sum += m1[i * k + l] * m2t[j * k + l];
-                            }
-
-                            local_block[i_rel][j_rel] += sum;
+                            __m512 a3 = _mm512_loadu_ps(&m1[i * k + l + 48]);
+                            __m512 b3 = _mm512_loadu_ps(&m2t[j * k + l + 48]);
+                            sum3 = _mm512_fmadd_ps(a3, b3, sum3);
                         }
+
+                        for (; l + 15 < k_end; l += 16) {
+                            __m512 a = _mm512_loadu_ps(&m1[i * k + l]);
+                            __m512 b = _mm512_loadu_ps(&m2t[j * k + l]);
+                            sum0 = _mm512_fmadd_ps(a, b, sum0);
+                        }
+
+                        float sum = _mm512_reduce_add_ps(
+                            _mm512_add_ps(_mm512_add_ps(sum0, sum1), _mm512_add_ps(sum2, sum3)));
+
+                        for (; l < k_end; l++) {
+                            sum += m1[i * k + l] * m2t[j * k + l];
+                        }
+
+                        local_block[i_rel][j_rel] += sum;
                     }
                 }
+            }
 
-                for (int i = i_block; i < std::min(i_block + BN, n); i++) {
-                    for (int j = j_block; j < std::min(j_block + BM, m); j++) {
-                        result[i * m + j] = local_block[i - i_block][j - j_block];
-                    }
+            for (int i = i_block; i < i_end; i++) {
+                for (int j = j_block; j < j_end; j++) {
+                    result[i * m + j] = local_block[i - i_block][j - j_block];
                 }
             }
         }
